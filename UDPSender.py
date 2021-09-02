55c5cd2f9d83fb98a9cb335c326a97d1026270a9
leokimcode <leokimcoding@gmail.com>
2021-09-02 16:13:51 -0400

    update

--- UDPSender.py
+++ UDPSender.py
@@ -9,11 +9,9 @@ message = "rams r3"
 localPort = 3241
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # this means we are using UDPcntrl questions mark for comment
-sock.bind(("", localPort)) #automatic ip, localPort
+sock.bind(('', UDP_PORT)) #automatic ip, localPort
 
-for x in range(1,100):
+while True:
     sock.sendto(bytes(message, "utf-8"), (UDP_IP, UDP_PORT)) #sending 
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes #receiving from the UDP_IP 
     print("received message: %s" % data) #printing the received in the terminal
-
-
